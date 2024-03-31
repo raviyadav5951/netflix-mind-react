29ac90f66891fe78d1e965670818c5d497f127fc
Ravi Yadav <raviyadav5951@gmail.com>
2024-03-31 18:29:57 +0530

    added multi lang feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from "react";
-import { LOGO, USER_AVATAR } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES, USER_AVATAR } from "../utils/constants";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../store/userSlice";
+import { toggleGptSearchView } from "../store/gptSlice";
+import { changeLanguage } from "../store/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const userFromStore = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   // console.log("userFromStore", userFromStore);
 
   useEffect(() => {
@@ -52,16 +56,42 @@ const Header = () => {
       })
       .catch((error) => {
         // An error happened.
-       // console.log("error in signout", error);
+        // console.log("error in signout", error);
         navigate("/error");
       });
+  };
+
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
   };
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img alt="logo" src={LOGO} className="w-44" />
       {userFromStore ? (
-        <div className="flex">
+        <div className="flex p-2 justify-between">
+           {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="mx-2 px-4 py-2 my-2 text-white bg-green-600"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch?"Home":"GPT Search"}
+          </button>
           <img
             alt="userlogo"
             src={userFromStore?.photoURL ?? USER_AVATAR}
